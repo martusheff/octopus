b41791788f03b5a75c20ddaa8274f2122fec0d23
Andronick Martusheff <71955336+martusheff@users.noreply.github.com>
2021-03-22 11:14:55 -0700

    initial
    curr: firebase realtime db, fb auth, user login (pod files not included)

---
+++ octopus/Controllers/ConversationsViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  octopus
+//
+//  Created by andronick martusheff on 3/18/21.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ConversationsViewController: UIViewController {
+
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.isHidden = true
+        table.register(UITableViewCell.self,
+                       forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private let noConversationsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "No Conversations!"
+        label.textAlignment = .center
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 21, weight: .medium)
+        label.isHidden = true
+        return label
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(tableView)
+        view.addSubview(noConversationsLabel)
+        setupTableView()
+        fetchConversations()
+        
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        validateAuth()
+        
+
+    }
+    private func validateAuth() {
+        if FirebaseAuth.Auth.auth().currentUser == nil {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
+    
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func fetchConversation() {
+        
+    }
+}
+
+extension ConversationsViewController: UITableViewDelegate, UITableViewDataSource
---
+++ octopus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  octopus
+//
+//  Created by andronick martusheff on 3/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
